6dded51d6c62234ba4adb75f31ff7d5c1c1b5bf5
Any97Cris <criscianysilva1997@gmail.com>
2023-06-24 09:08:18 -0400

    class model

---
+++ app/database/models/Model.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\database\models;
+
+use app\database\Connection;
+
+abstract class Model{
+
+    protected string $table;
+
+    public function all(string $fields = '*'){
+        try{
+            // $connection = Connection::getConnection();
+            $sql = "select {$fields} from {$this->table}";
+            var_dump($sql);
+            // $query = $connection->query($sql);
+            // return $query->fetchAll();
+        }catch(\PDOException $e){
+            var_dump($e->getMessage());
+        }
+    }
+}
+
+?>
