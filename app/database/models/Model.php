7b8daf8a0a4d11fc2da968517f99dd3ba068bc5b
Any97Cris <criscianysilva1997@gmail.com>
2023-06-26 08:51:14 -0400

    metodos

--- app/database/models/Model.php
+++ app/database/models/Model.php
@@ -2,18 +2,33 @@
 namespace app\database\models;
 
 use app\database\Connection;
+use PDO;
 
 abstract class Model{
 
     protected string $table;
+    private array $attributes = [];
+
+    public function __set($key, $value){
+        if(!isset($this->attributes[$key])){
+            $this->attributes[$key] = $value;
+        }
+    }
+
+    public function __get($key){
+        return $this->attributes[$key];
+    }
+
+    public function toJson(array $value){
+        echo json_encode($value);
+    }
 
     public function all(string $fields = '*'){
         try{
-            // $connection = Connection::getConnection();
+            $connection = Connection::getConnection();
             $sql = "select {$fields} from {$this->table}";
-            var_dump($sql);
-            // $query = $connection->query($sql);
-            // return $query->fetchAll();
+            $query = $connection->query($sql);
+            return $query->fetchAll(PDO::FETCH_CLASS, static::class);
         }catch(\PDOException $e){
             var_dump($e->getMessage());
         }
